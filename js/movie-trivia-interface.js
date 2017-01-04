78f9526832c85e93cdb11b2f937e6e4ed4fafb83
Claire Sial and Chris Wingo <chris@>
2017-01-04 13:11:26 -0800

    Error checking working

--- js/movie-trivia-interface.js
+++ js/movie-trivia-interface.js
@@ -2,9 +2,13 @@ $(document).ready(function() {
   $('#movieButton').click(function() {
     var movie = $('#movie').val();
     $('#movie').val("");
-    $('.showMovie').text("The movie you have chosen is " + movie + ".");
-    $.get('http://www.omdbapi.com/?t=' + movie, function(response) {
-      console.log(response);
+    $.get('http://www.omdbapi.com/?t=' + movie).then(function(response) {
+      if(response.Response == "True") {
+        $('.showMovie').text("The movie you have chosen is " + response.Title + ".");
+        console.log(JSON.stringify(response));
+      } else {
+        $('.showMovie').text(response.Error);
+      }
     });
   });
 });
