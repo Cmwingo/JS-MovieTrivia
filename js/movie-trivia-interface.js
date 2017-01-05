ca3792a178935f002d68f3cd3f426fd2ebad29c1
Claire Sial and Chris Wingo <chris@>
2017-01-04 16:38:53 -0800

    Done for the day

--- js/movie-trivia-interface.js
+++ js/movie-trivia-interface.js
@@ -1,6 +1,6 @@
 var Movie = require('./../js/movie-trivia.js').movieModule;
 
-var displayTitle = function(title, movieTitle, movieDirector, movieYear, movieActors) {
+var displayMovie = function(title, movieTitle, movieDirector, movieYear, movieActors) {
   $('.showMovie').text(movieTitle + " by " + movieDirector + ": " + movieYear + " starring ");
   var movieActorArray = movieActors.split(",");
   movieActorArray.forEach(function(actor){
@@ -11,8 +11,10 @@ var displayTitle = function(title, movieTitle, movieDirector, movieYear, movieAc
 $(document).ready(function() {
   var currentMovieObject = new Movie();
   $('#movieButton').click(function() {
+    var movieObject = new Movie();
     var movie = $('#movie').val();
     $('#movie').val("");
-    currentMovieObject.getMovie(movie, displayTitle);
+    currentMovieObject = movieObject.getMovie(movie, displayMovie);
+    debugger;
   });
 });
